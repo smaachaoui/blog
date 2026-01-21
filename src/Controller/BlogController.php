b8fb84590640534bd4d0a96b269f65b8a39d0bfd
seif <smaachaoui@outlook.com>
2026-01-21 16:52:05 +0100

    J'importe ma classe Comment, mon formulaire CommentType et mon CommentRepository + J'apporte des modifications à ma méthode show en incluant la création et la soumission d'un commentaire

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,10 +4,20 @@ namespace App\Controller;
 
 use App\Entity\Post;
 use App\Repository\PostRepository;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+
+use App\Entity\Comment;
+use App\Form\CommentType;
+use App\Repository\CommentRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+
+
+
+
 
 class BlogController extends AbstractController
 {
@@ -34,18 +44,39 @@ class BlogController extends AbstractController
     }
 
 
-    #[Route('/blog/{slug}', name: 'blog_show', methods: ['GET'])]
-    public function show(PostRepository $postRepository, string $slug): Response
-    {
+    #[Route('/blog/{slug}', name: 'blog_show', methods: ['GET', 'POST'])]
+    public function show(
+        PostRepository $postRepository,
+        CommentRepository $commentRepository,
+        EntityManagerInterface $entityManager,
+        Request $request,
+        string $slug
+    ): Response {
         $post = $postRepository->findOneBy(['slug' => $slug]);
         if ($post === null) {
             throw $this->createNotFoundException();
         }
 
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setPost($post);
+            $entityManager->persist($comment);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Commentaire envoyé. Il sera visible après validation.');
+            return $this->redirectToRoute('blog_show', ['slug' => $slug], Response::HTTP_SEE_OTHER);
+        }
+
         return $this->render('blog/show.html.twig', [
             'post' => $post,
+            'comments' => $commentRepository->findApprovedForPost($post->getId()),
+            'commentForm' => $form,
         ]);
     }
+
 
 }
 
