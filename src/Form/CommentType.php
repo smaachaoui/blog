c80cdb55cb1b6bbe7b48966332f9a5e8b102e251
seif <smaachaoui@outlook.com>
2026-01-21 16:44:33 +0100

    Rajout d'un formulaire pour pouvoir écrire les commentaires + je précise les champs de ma table Comment dans ce formulaire

---
+++ src/Form/CommentType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Comment;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('authorName')
+            ->add('authorEmail', EmailType::class, [
+                'required' => false,
+            ])
+            ->add('content', TextareaType::class, [
+                'attr' => [
+                    'rows' => 4,
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Comment::class,
+        ]);
+    }
+}
