75e679f296adda29f98dd0f0ecf8d2a9ac7d350d
seif <smaachaoui@outlook.com>
2026-01-21 15:53:55 +0100

    Modification de la méthode home : Rajout de la lecture des paramètres d'url avec  et , je définis la pagination avec  et . Ensuite avec  je fais une recherche qui trie par date décroissante, filtre si  existe et limite les resultats. Puis je passe au comptage des résultat avec  et enfin je transmets les données à la template twig

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -12,12 +12,27 @@ use Symfony\Component\HttpFoundation\Request;
 class BlogController extends AbstractController
 {
     #[Route('/', name: 'app_home', methods: ['GET'])]
-    public function home(PostRepository $postRepository): Response
+    public function home(Request $request, PostRepository $postRepository): Response
     {
+        $page = max(1, $request->query->getInt('page', 1));
+        $query = $request->query->get('q');
+
+        $limit = 6;
+        $offset = ($page - 1) * $limit;
+
+        $posts = $postRepository->search($query, $limit, $offset);
+        $total = $postRepository->countSearch($query);
+
+        $totalPages = (int) ceil($total / $limit);
+
         return $this->render('blog/index.html.twig', [
-            'posts' => $postRepository->findBy([], ['createdAt' => 'DESC'], 10),
+            'posts' => $posts,
+            'currentPage' => $page,
+            'totalPages' => $totalPages,
+            'query' => $query,
         ]);
     }
+
 
     #[Route('/blog/{slug}', name: 'blog_show', methods: ['GET'])]
     public function show(PostRepository $postRepository, string $slug): Response
