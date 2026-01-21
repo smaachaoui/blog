faaed71d48240da4ec820a0e82af7058b6c023e5
seif <smaachaoui@outlook.com>
2026-01-21 16:59:56 +0100

    Je génère un crud sur mon entité Comment

---
+++ src/Form/Comment1Type.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Comment;
+use App\Entity\Post;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Comment1Type extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('authorName')
+            ->add('authorEmail')
+            ->add('content')
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('isApproved')
+            ->add('post', EntityType::class, [
+                'class' => Post::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Comment::class,
+        ]);
+    }
+}
