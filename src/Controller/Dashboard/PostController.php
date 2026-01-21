0b4863689897c6cf5557f9c5915111c68eaffef2
seif <smaachaoui@outlook.com>
2026-01-21 14:42:49 +0100

    Mise en place de l'espace utilisateur : Je commence par créer un Postcontroller qui sera dans un dossier Dashboard + je configure les routes de mon fichier Dashboard/PostController.php avec le fait qu'il faut avoir le rôle USER pour y accéder (donc être un utilisateur connecté). Dans ma méthode index, l'utilisateur ne verra que ses propres post (fonction findBy) + Je complète mon template index.html.twig dans dashboard et je créer deux autres template qui sont new.html.twig et edit.html.twig

---
+++ src/Controller/Dashboard/PostController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Controller\Dashboard;
+
+use App\Entity\Post;
+use App\Form\PostType;
+use App\Repository\PostRepository;
+use App\Security\Voter\PostVoter;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('ROLE_USER')]
+#[Route('/dashboard/posts')]
+class PostController extends AbstractController
+{
+    #[Route('/', name: 'dashboard_post_index', methods: ['GET'])]
+    public function index(PostRepository $postRepository): Response
+    {
+        $user = $this->getUser();
+
+        return $this->render('dashboard/post/index.html.twig', [
+            'posts' => $postRepository->findBy(['author' => $user], ['createdAt' => 'DESC']),
+        ]);
+    }
+
+    #[Route('/new', name: 'dashboard_post_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $this->denyAccessUnlessGranted(PostVoter::CREATE);
+
+        $post = new Post();
+        $post->setAuthor($this->getUser());
+
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($post);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('dashboard_post_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('dashboard/post/new.html.twig', [
+            'post' => $post,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'dashboard_post_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Post $post, EntityManagerInterface $entityManager): Response
+    {
+        $this->denyAccessUnlessGranted(PostVoter::EDIT, $post);
+
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post->setUpdatedAt(new \DateTimeImmutable());
+            $entityManager->flush();
+
+            return $this->redirectToRoute('dashboard_post_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('dashboard/post/edit.html.twig', [
+            'post' => $post,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'dashboard_post_delete', methods: ['POST'])]
+    public function delete(Request $request, Post $post, EntityManagerInterface $entityManager): Response
+    {
+        $this->denyAccessUnlessGranted(PostVoter::DELETE, $post);
+
+        if ($this->isCsrfTokenValid('delete'.$post->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($post);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('dashboard_post_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
