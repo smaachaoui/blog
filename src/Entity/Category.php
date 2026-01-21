41cceafbf14b64756690606c6c248c9c92bfa912
seif <smaachaoui@outlook.com>
2026-01-21 11:00:35 +0100

    Je créer mon entité Category avec les champs name et slug + je modifie l'entité en rajoutant une contrainte d'unicité sur le champ slug

---
+++ src/Entity/Category.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+
+#[ORM\Entity]
+#[ORM\Table(name: 'category')]
+#[ORM\UniqueConstraint(name: 'uniq_category_slug', columns: ['slug'])]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 120)]
+    private ?string $slug = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): static
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+}
