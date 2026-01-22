3a31bcd0a82b81c4177e7018ebe76b99657ffdb6
seif <smaachaoui@outlook.com>
2026-01-22 15:27:27 +0100

    Je créer un controller pour le profil utilisateur, puis je créer son template et enfin je rajoute un lien vers mon profil dans le header

---
+++ src/Controller/Dashboard/ProfileController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Dashboard;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('ROLE_USER')]
+class ProfileController extends AbstractController
+{
+    #[Route('/profile', name: 'app_profile', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('dashboard/profile/index.html.twig');
+    }
+}
