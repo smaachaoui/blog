99531abcadbb5ed3897140c583a75f96d2e8bdc4
seif <smaachaoui@outlook.com>
2026-01-22 09:17:36 +0100

    Je rajoute un champ coverImageFile non mappé avec des contraintes liés aux format des images et à sa taille maximum

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -9,10 +9,13 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
+
 
 class PostType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
+   public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('title')
@@ -23,6 +26,22 @@ class PostType extends AbstractType
                     'rows' => 10,
                 ],
             ])
+            ->add('coverImageFile', FileType::class, [
+                'mapped' => false,
+                'required' => false,
+                'label' => 'Couverture (jpg, png, webp)',
+                'constraints' => [
+                    new File([
+                        'maxSize' => '2M',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/webp',
+                        ],
+                        'mimeTypesMessage' => 'Veuillez envoyer une image valide (jpg, png, webp).',
+                    ]),
+                ],
+            ])
             ->add('category', EntityType::class, [
                 'class' => Category::class,
                 'choice_label' => 'name',
@@ -30,10 +49,4 @@ class PostType extends AbstractType
             ]);
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefaults([
-            'data_class' => Post::class,
-        ]);
-    }
 }
